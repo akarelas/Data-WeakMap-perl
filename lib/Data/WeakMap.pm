5dbc22d10acfb1efe030d6ae9cfe799adb2dbc31
Alexander Karelas <karjala@cpan.org>
2020-04-09 23:19:47 +0000

    initial commit

---
+++ lib/Data/WeakMap.pm
@@ -0,0 +1,39 @@
+package Data::WeakMap;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::WeakMap - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::WeakMap;
+
+=head1 DESCRIPTION
+
+Data::WeakMap is ...
+
+=head1 LICENSE
+
+Copyright (C) Alexander Karelas.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Alexander Karelas E<lt>karjala@cpan.orgE<gt>
+
+=cut
+
