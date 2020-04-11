9b0460c04fdebad3e02e8e33fceafa221ad41108
Alexander Karelas <karjala@cpan.org>
2020-04-11 06:48:34 +0000

    Complete rewrite into an easy-to-reason about small tidy code

--- cpanfile
+++ cpanfile
@@ -1,6 +1,9 @@
-requires 'perl', '5.014';
+requires 'perl', '5.014'; # need access to the ${^GLOBAL_PHASE} var
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
+    requires 'Test::Deep'; # TODO: find minimum version for this
 };
 
+requires 'Scalar::Util';
+requires 'Carp';
---
+++ lib/Data/ManBearPig.pm
@@ -0,0 +1,26 @@
+package Data::ManBearPig;
+use 5.014;
+use strict;
+use warnings FATAL => 'all';
+
+our $VERSION = "v0.0.2";
+
+use overload '%{}'  => sub { ${$_[0]}->{dummy} }, fallback => 1;
+use overload 'bool' => sub { 1 }, fallback => 1;
+
+sub new {
+    my ($class) = @_;
+
+    my $self = \{
+        tied_keys => {},
+        values    => {},
+        dummy     => {},
+    };
+
+    require Data::WeakMap::Tie;
+    tie %{ $$self->{dummy} }, 'Data::WeakMap::Tie', $self;
+
+    bless $self, $class;
+}
+
+1;
